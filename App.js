2266f2d02979834074520ddb0811aed7e484bb93
Carlos Iriarte <carlos.iriarte@amerikatis.com>
2023-04-28 19:21:00 -0500

    Primer Commit Prueba

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenidos a la primera vista de QuillaEcommerce con React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
